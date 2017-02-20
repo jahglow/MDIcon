fd9afac0d62dc3736a03a968b07236e216d95c17
Ivan Pilyugin <jahglow@gmail.com>
2017-02-20 18:08:48 +0300

    refactor

--- src/MDIcon.js
+++ src/MDIcon.js
@@ -1,23 +1,20 @@
 /**
  * Created by IvanP on 23.01.2017.
  */
-import React from 'react';
-export class MDIcon extends React.Component {
-  /**
-   * Renders an SVG material icon imported from an icon-set and passed via props
-   * @param {Object} props
-   * @param {Object} props.icon - imported icon definition (jsx)
-   * @param {Object} props.children - imported icon definition (jsx) if you prefer using children and a closing tag in JSX rather than an icon attribute
-   * @param {Number} [props.size=24] - icon width/height (which equals height since all MD icons have square viewbox). Any of `18|24|36|48`
-   * @param {String} [props.fill='rgba(0,0,0,.85)'] - icon fill - any valid css color, fills the whole icon
-   * */
-  constructor(props){
-    super(props);
-  }
+import React, {PureComponent} from 'react';
+/**
+ * Renders an SVG material icon imported from an icon-set and passed via props
+ * @param {Object} props
+ * @param {Object} props.icon - imported icon definition (jsx)
+ * @param {Object} props.children - imported icon definition (jsx) if you prefer using children and a closing tag in JSX rather than an icon attribute
+ * @param {Number} [props.size=24] - icon width/height (which equals height since all MD icons have square viewbox). Any of `18|24|36|48`
+ * @param {String} [props.fill='rgba(0,0,0,.85)'] - icon fill - any valid css color, fills the whole icon
+ * */
+export class MDIcon extends PureComponent {
   render() {
-    return this.props.icon || this.props.children? (
-      <svg xmlns="http://www.w3.org/2000/svg" fill={this.props.fill} width={this.props.size} height={this.props.size} viewBox="0 0 24 24">{this.props.icon?this.props.icon:this.props.children?this.props.children:null}</svg>
-    ) : null;
+    let {fill,size,icon,children} = this.props;
+    console.log(fill,size,icon,children);
+    return (<svg xmlns="http://www.w3.org/2000/svg" fill={fill} width={size} height={size} viewBox="0 0 24 24" children={icon || children} />);
   }
 }
 
